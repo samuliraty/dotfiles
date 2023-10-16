2115fd21ff2c8175de8c8f405808c4ae66d05d03
Samuli Räty <samuli.raty@gmail.com>
2023-10-16 21:19:24 +0300

    Revert to standard macos layout

--- .hammerspoon/init.lua
+++ .hammerspoon/init.lua
@@ -1,11 +1,11 @@
-hs.hotkey.bind({"ctrl"}, "§", function()
+hs.hotkey.bind({"cmd"}, "§", function()
     hs.execute("yabai -m window --toggle float", true)
 end)
 
-hs.hotkey.bind({"ctrl"}, "1", function()
+hs.hotkey.bind({"cmd"}, "1", function()
   hs.application.launchOrFocus("iTerm")
 end)
 
-hs.hotkey.bind({"ctrl"}, "2", function()
+hs.hotkey.bind({"cmd"}, "2", function()
   hs.application.launchOrFocus("Google Chrome")
 end)
