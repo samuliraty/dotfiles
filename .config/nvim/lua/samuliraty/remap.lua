b2502f9a8e30be952e34c58622e618505da7b848
Samuli Räty <samuli.raty@gmail.com>
2023-10-20 15:55:40 +0300

    Add a shortcut for 'save and exit to netrw'

--- .config/nvim/lua/samuliraty/remap.lua
+++ .config/nvim/lua/samuliraty/remap.lua
@@ -62,3 +62,7 @@ vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><
 
 -- Make the current file executable
 vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
+
+-- Save and switch to netrw
+vim.api.nvim_set_keymap('n', 'ZZ', ':w<CR>:e .<CR>', { noremap = true, silent = true })
+vim.cmd[[ command! WQ w | e . ]]
