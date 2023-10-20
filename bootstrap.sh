8c156e935946d76b32975035d06bfb8beee48c0b
Samuli Räty <samuli.raty@gmail.com>
2023-10-20 14:57:54 +0300

    Move linuxbrew installation to readme.md

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,24 +2,14 @@
 
 echo "🧙 I solemnly swear that I am up to no good. 🪄✨"
 
-# Install Homebrew, Zsh, and set as default shell if not on macOS
-if [[ "$(uname)" != "Darwin" ]]; then
-  if ! command -v brew &> /dev/null; then
-    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-    eval "$(brew --prefix)/bin/brew shellenv"
-    ln -s /home/linuxbrew/.linuxbrew/bin/brew /usr/local/bin/brew
-  fi
+if [[ -n "$WSL_DISTRO_NAME" ]]; then
+  brew install zsh
+  chsh -s "$(which zsh)"
 
-  if ! command -v zsh &> /dev/null; then
-    brew install zsh
-    chsh -s "$(which zsh)"
-  fi
 fi
 
-# Install packages via Homebrew
 brew install powerlevel10k nvm fzf zsh-autosuggestions zsh-syntax-highlighting tmux nvim
 
-# Create a marker file
 touch ~/.bootstrap_done
 
 echo "🧙 Mischief managed. 🪄✨"
