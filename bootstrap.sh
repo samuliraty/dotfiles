2053098e29ebcae95d6c1869d05d34729a9a6474
Samuli Räty <samuli.raty@gmail.com>
2023-10-21 20:17:05 +0300

    Fix oh-my-zsh installation overwriting .zshrc

--- bootstrap.sh
+++ bootstrap.sh
@@ -7,7 +7,7 @@ if [[ "$WSL_DISTRO_NAME" == "Ubuntu" ]]; then
   eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
   sudo apt-get install build-essential
   brew install gcc zsh
-  sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" --keep-zshrc
+  sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --keep-zshrc
 fi
 
 brew install powerlevel10k nvm fzf zsh-autosuggestions zsh-syntax-highlighting tmux nvim
