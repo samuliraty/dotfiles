b765a51b9c8543008e33ca3620c40d4b8d77b640
Samuli Räty <samuli.raty@gmail.com>
2023-10-21 12:46:07 +0300

    Add a shortcut for find in all files

--- .config/nvim/after/plugin/telescope.lua
+++ .config/nvim/after/plugin/telescope.lua
@@ -1,5 +1,14 @@
 local builtin = require('telescope.builtin')
 vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
+vim.keymap.set('n', '<leader>fa', function()
+    builtin.find_files({
+        search_dirs = { "~", "~/repos", "~/.config" },
+        prompt_title = "< Home | Repos | Config >",
+        hidden = true, -- to find hidden files as well
+        follow = true, -- to resolve symlinks
+        ignore = { "node_modules" } -- add other directories to exclude if needed
+    })
+end, {})
 vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
 vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
 vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
