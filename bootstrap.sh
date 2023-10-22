f831ef77b9231c29e152eff1d81ee3de5921c22d
Samuli Räty <samuli.raty@gmail.com>
2023-10-22 21:42:06 +0300

    Add zsh to list of valid shells on Linux

--- bootstrap.sh
+++ bootstrap.sh
@@ -7,6 +7,7 @@ if [[ "$WSL_DISTRO_NAME" == "Ubuntu" ]]; then
   eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
   sudo apt-get install build-essential
   brew install zsh gcc
+  echo '/home/linuxbrew/.linuxbrew/bin/zsh' | sudo tee -a /etc/shells
 fi
 
 brew install powerlevel10k nvm fzf zsh-autosuggestions zsh-syntax-highlighting tmux nvim firefox
