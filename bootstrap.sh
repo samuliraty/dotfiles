8b50a00104f5b5772585bc4bf530c1fda09ccfbe
Samuli Räty <32360408+samuliraty@users.noreply.github.com>
2023-10-23 10:09:44 +0300

    Add llvm

--- bootstrap.sh
+++ bootstrap.sh
@@ -5,8 +5,8 @@ echo "🧙 I solemnly swear that I am up to no good. 🪄✨"
 if [[ "$WSL_DISTRO_NAME" == "Ubuntu" ]]; then
   /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
   eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
+  brew install zsh gcc llvm
   sudo apt-get install build-essential
-  brew install zsh gcc
   echo '/home/linuxbrew/.linuxbrew/bin/zsh' | sudo tee -a /etc/shells
   gcc_version=$(ls /home/linuxbrew/.linuxbrew/bin | grep -Eo 'gcc-[^-]+' | cut -d- -f2 | sort -V | head -n1)
   ln -s "/home/linuxbrew/.linuxbrew/bin/gcc-$gcc_version" "/home/linuxbrew/.linuxbrew/bin/gcc"
