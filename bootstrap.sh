05083a4a9082526a8d631306f1dd9e1d5758f979
Samuli Räty <32360408+samuliraty@users.noreply.github.com>
2023-10-23 09:37:28 +0300

    Fix gcc path

--- bootstrap.sh
+++ bootstrap.sh
@@ -7,9 +7,9 @@ if [[ "$WSL_DISTRO_NAME" == "Ubuntu" ]]; then
   eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
   sudo apt-get install build-essential
   brew install zsh gcc
-  gcc_version=$(ls /home/linuxbrew/.linuxbrew/bin | grep -E ^gcc-[0-9]+$ | sort -V | tail -n1)
-  ln -s "/home/linuxbrew/.linuxbrew/bin/$gcc_version" "/home/linuxbrew/.linuxbrew/bin/gcc"
   echo '/home/linuxbrew/.linuxbrew/bin/zsh' | sudo tee -a /etc/shells
+  gcc_version=$(find /home/linuxbrew/.linuxbrew/bin -type f -name 'gcc-*' | grep -E 'gcc-[0-9]+$' | sort -V | tail -n1)
+  ln -s "$gcc_version" "/home/linuxbrew/.linuxbrew/bin/gcc"
 fi
 
 brew install powerlevel10k nvm fzf zsh-autosuggestions zsh-syntax-highlighting tmux nvim lazygit gh
