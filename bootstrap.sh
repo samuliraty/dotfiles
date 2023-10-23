44aff31e5d99e8f012e633bf05c5aa2761f7510f
Samuli Räty <32360408+samuliraty@users.noreply.github.com>
2023-10-23 09:13:21 +0300

    Add gcc to path

--- bootstrap.sh
+++ bootstrap.sh
@@ -7,6 +7,8 @@ if [[ "$WSL_DISTRO_NAME" == "Ubuntu" ]]; then
   eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
   sudo apt-get install build-essential
   brew install zsh gcc
+  gcc_version=$(ls /home/linuxbrew/.linuxbrew/bin | grep -E ^gcc-[0-9]+$ | sort -V | tail -n1)
+  ln -s "/home/linuxbrew/.linuxbrew/bin/$gcc_version" "/home/linuxbrew/.linuxbrew/bin/gcc"
   echo '/home/linuxbrew/.linuxbrew/bin/zsh' | sudo tee -a /etc/shells
 fi
 
