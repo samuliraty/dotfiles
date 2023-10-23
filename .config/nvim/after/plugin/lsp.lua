6721c21f3b55c2a8f2249fb6b48f95560109eafd
Samuli Räty <32360408+samuliraty@users.noreply.github.com>
2023-10-23 09:37:52 +0300

    Add bashls

--- .config/nvim/after/plugin/lsp.lua
+++ .config/nvim/after/plugin/lsp.lua
@@ -8,7 +8,7 @@ end)
 
 require('mason').setup({})
 require('mason-lspconfig').setup({
-  ensure_installed = {'tsserver', 'lua_ls'},
+  ensure_installed = {'tsserver', 'lua_ls', 'bashls'},
   handlers = {
     lsp_zero.default_setup,
     lua_ls = function()
